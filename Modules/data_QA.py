ce417589838deb08fd278fc02be869828268e907
Atharva Thakur <atharvathakur369@gmail.com>
2024-05-24 20:14:25 +0000

    Code gen and runner added

--- Modules/data_QA.py
+++ Modules/data_QA.py
@@ -5,17 +5,31 @@ import pandas as pd
 from dotenv import load_dotenv
 import os
 
+from Modules.data_code_run import DataCodeRun
+from Modules.python_interpreter import PythonInterpreter, run_interpreter
+
 load_dotenv()  # take environment variables from .env.
 
 class DataQA:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self):
+        print("dataQA")
+    # def ask_csv(self):
+    #     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+    #     llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY)
+    #     csv_agent = create_csv_agent(llm,"data.csv", verbose=True)
+    #     question = st.text_input("Ask your question:")
+    #     if question:
+    #         response = csv_agent.invoke(question)
+    #         st.write(response)
 
     def ask_csv(self):
-        GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-        llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY)
-        csv_agent = create_csv_agent(llm,"data.csv", verbose=True)
         question = st.text_input("Ask your question:")
+        code_runner = DataCodeRun()
         if question:
-            response = csv_agent.invoke(question)
-            st.write(response)
+            response= code_runner.generate_code(question)
+            plan, python_code = code_runner.extract_code(response)
+            st.write(plan)
+            st.code(python_code)
+            if st.button("Run Code") and python_code:
+                interpreter_code_output = run_interpreter(python_code)
+                print("Python code output:\n", interpreter_code_output)
